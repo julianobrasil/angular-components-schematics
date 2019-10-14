ca73c4178d5dbd6c43e427abcc6cff428bf6e2de
Juliano <juliano.brasil@gmail.com>
2019-10-14 00:12:45 -0300

    feat: add login module schematics

---
+++ components-schematics/projects/components/schematics/login-module/schema.ts
@@ -0,0 +1,13 @@
+export interface Schema {
+  // The name of the module.
+  name: string;
+
+  // The path to create the module.
+  path?: string;
+
+  // The name of the project.
+  project?: string;
+
+  /** Wether to implements control value accessor interface */
+  controlValueAccessor?: boolean;
+}
