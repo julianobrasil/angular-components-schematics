e1e1fb7e28a9eaaba1312a4bfc20466ed8ef3d64
Juliano <juliano.brasil@gmail.com>
2019-12-04 18:28:55 -0300

    build: add types and VSCode plugins (#9)

--- components-schematics/projects/components/src/lib/components.service.spec.ts
+++ components-schematics/projects/components/src/lib/components.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ComponentsService } from './components.service';
+import {ComponentsService} from './components.service';
 
 describe('ComponentsService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
